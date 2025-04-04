ed49e2400a4d21aae4a3ce68014a64761790bc81
Didier Marty <boutcamp@gmail.com>
2025-04-04 16:41:52 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/**/*.{html,js}","*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Montserrat:['Montserrat'],
+        Raleway:['Raleway'],
+      }
+    },
+  },
+  plugins: [],
+}
+
